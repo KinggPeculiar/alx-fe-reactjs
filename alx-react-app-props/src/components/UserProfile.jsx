1a46ddd4d776f95c3dad66f2439fac56dda12f70
KinggPeculiar <KingsleyPeculiar04@gmail.com>
2025-09-07 16:06:19 +0100

    fix: update UserProfile.jsx to remove use of props and consume UserContext inside the component

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,15 @@
-import { div } from "three/tsl";
+import { useContext } from "react";
+import UserContext from "../javascript/UserContext"; // adjust path if needed
 
-function UserProfile(props) {
-    return(
-       <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-       </div> 
+function UserProfile() {
+    const userData = useContext(UserContext); // ✅ pull data from context
+
+    return (
+        <div>
+            <h2>{userData.name}</h2>
+            <p>Age: {userData.age}</p>
+            <p>Bio: {userData.bio}</p>
+        </div>
     );
 }
 
