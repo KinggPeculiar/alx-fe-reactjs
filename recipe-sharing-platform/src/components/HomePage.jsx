7e4b309b92ea9364ca0a3bf3d8315d5fd8c34f6e
KinggPeculiar <KingsleyPeculiar04@gmail.com>
2025-10-04 16:23:39 +0100

    fix: set div to grid-col-1

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -15,7 +15,7 @@ function HomePage() {
           Welcome to Recipe Finder
         </h1>
       
-        <div className="grid grid-rows-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {recipes.map((recipe) => (
                 <div
                   key={recipe.id}
