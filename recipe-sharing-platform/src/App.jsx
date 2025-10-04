ac01abc596da44764dd92550e6bbdfea87cedc55
KinggPeculiar <KingsleyPeculiar04@gmail.com>
2025-10-04 16:21:08 +0100

    feat: Initialize HomePage component using useState and useEffect hooks to load recipe data from data.json file into state. - style home page with tailwindcss - Implement Responsive layout with grid display

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import HomePage from './components/HomePage'
 import './App.css'
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <h1 className=' text-red-500 '>Hello World</h1>
+      <HomePage />
     </div>
   )
 }
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import recipeData from "../data.json"; // relative path
+
+function HomePage() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    setRecipes(recipeData);
+  }, []);
+
+  return (
+    <div className="p-6 max-w-6xl mx-auto flex flex-col items-center justify-center">
+        {/* Page Title */}
+        <h1 className="text-4xl font-extrabold mb-10 text-center text-indigo-600">
+          Welcome to Recipe Finder
+        </h1>
+      
+        <div className="grid grid-rows-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {recipes.map((recipe) => (
+                <div
+                  key={recipe.id}
+                  className="bg-white border rounded-xl shadow-md hover:shadow-xl transform hover:scale-105 transition duration-300"
+                >
+                    {/* Recipe Image */}
+                    <img 
+                      src={recipe.image}
+                      alt={recipe.title}
+                      className="w-full h-48 object-cover rounded-t-xl"
+                    />
+                    {/* Card Content */}
+                    <div className="p-4">
+                        <h2 className="text-2xl font-semibold mb-2 text-gray-800">
+                            {recipe.title}
+                        </h2>
+                        <p className="text-gray-600 text-sm">
+                            {recipe.summary}
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+  );
+}
+
+export default HomePage;
